459da0caba69504008d0d75d4e91aee3e8dae166
Khanh N. Dang <khanhdang@users.noreply.github.com>
2018-11-21 16:39:25 +0700

    Upload first version

---
+++ SW/.sdk/launch_scripts/xilinx_c-c++_application_(system_debugger)/debug_rtos.tcl
@@ -0,0 +1,16 @@
+connect -url tcp:127.0.0.1:3121
+targets -set -filter {jtag_cable_name =~ "Digilent JTAG-SMT1 210203A3CFF3A" && level==0} -index 0
+fpga -file F:/Project/FPGA/HDMI_AC701/ac701_project_dual/adv7511_ac701.sdk/system_top_hw_platform_1/download.bit
+targets -set -nocase -filter {name =~ "microblaze*#1" && bscan=="USER2"  && jtag_cable_name =~ "Digilent JTAG-SMT1 210203A3CFF3A"} -index 0
+loadhw F:/Project/FPGA/HDMI_AC701/ac701_project_dual/adv7511_ac701.sdk/system_top_hw_platform_1/system.hdf
+targets -set -nocase -filter {name =~ "microblaze*#1" && bscan=="USER2"  && jtag_cable_name =~ "Digilent JTAG-SMT1 210203A3CFF3A"} -index 0
+rst -system
+after 3000
+targets -set -nocase -filter {name =~ "microblaze*#1" && bscan=="USER2"  && jtag_cable_name =~ "Digilent JTAG-SMT1 210203A3CFF3A"} -index 0
+dow F:/Project/FPGA/HDMI_AC701/ac701_project_dual/adv7511_ac701.sdk/driver_HDMI/Debug/driver_HDMI.elf
+targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent JTAG-SMT1 210203A3CFF3A"} -index 0
+dow F:/Project/FPGA/HDMI_AC701/ac701_project_dual/adv7511_ac701.sdk/test_RTOS/Debug/test_RTOS.elf
+targets -set -nocase -filter {name =~ "microblaze*#1" && bscan=="USER2"  && jtag_cable_name =~ "Digilent JTAG-SMT1 210203A3CFF3A"} -index 0
+con
+targets -set -nocase -filter {name =~ "microblaze*#0" && bscan=="USER2"  && jtag_cable_name =~ "Digilent JTAG-SMT1 210203A3CFF3A"} -index 0
+con
